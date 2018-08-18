416337f2f5c5a0281f15a695bd0bb1ed4138ae35
chang <changlee99@gmail.com>
2018-08-18 11:24:27 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <header class="app-header">
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark"  style="margin-bottom: 20px">
+        <a class="navbar-brand" href="#">
+          <img src="./assets/logo.png" width="30" height="30" alt="">
+          Test
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mr-auto" >
+            <li class="nav-item">
+              <router-link class="nav-link" exact-active-class="active" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" active-class="active" to="/about">About</router-link>
+            </li>
+          </ul>  
+        </div>
+      </nav>
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+		<div class="card border-primary mb-3">
+		  <div class="card-header">About</div>
+		  <div class="card-body text-secondary">
+		    <h5 class="card-title">Vue+Bootrap Templete</h5>
+		    <p class="card-text">
+		    	This project using Vue, Bootstrap, web3,<br/>
+			    Some real working project based on this templete is <a href="https://github.com/lcw99/contract-manager">here</a>
+		    </p>
+		  </div>
+		</div>		
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
